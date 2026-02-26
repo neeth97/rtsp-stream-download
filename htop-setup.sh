197b29488c41625d7373906f926f3e63e87eecba
Praneeth K <100612144+neeth97@users.noreply.github.com>
2026-02-26 14:22:00 +0530

    Update htop-setup.sh

--- htop-setup.sh
+++ htop-setup.sh
@@ -1,9 +1,9 @@
-wget https://github.com/htop-dev/htop/archive/refs/tags/3.2.2.tar.gz
-tar -xzf 3.2.2.tar.gz
-cd htop-3.2.2
-./autogen.sh
-./configure --prefix=$HOME/.local
-make
-make install
+wget https://github.com/htop-dev/htop/releases/download/3.2.2/htop-3.2.2-x86_64-linux-gnu.tar.xz
+tar -xf htop-3.2.2-x86_64-linux-gnu.tar.xz
+cd htop-3.2.2-x86_64-linux-gnu
+chmod +x htop
+mkdir -p ~/.local/bin
+mv htop ~/.local/bin/
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
+htop
