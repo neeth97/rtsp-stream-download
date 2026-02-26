3c99bbe02de2bf96a6f443063edb9a9726088a35
Praneeth K <100612144+neeth97@users.noreply.github.com>
2026-02-27 00:00:10 +0530

    Update and rename aws-file-copy.sh to aws-night-videos-copy.sh

--- aws-file-copy.sh
+++
@@ -1,4 +0,0 @@
-aws s3 sync \
-~/Desktop/rtsp-stream-download-main/25022026_1142AM_0542PM \
-s3://metroinfrasys-images-gantry/02-25-2026+videos/ \
---dryrun
---
+++ aws-night-videos-copy.sh
@@ -0,0 +1,3 @@
+aws s3 sync \
+~/Desktop/rtsp-stream-download-main/25022026_0609PM_1139PM \
+"s3://metroinfrasys-images-gantry/02-25-2026 videos/"
